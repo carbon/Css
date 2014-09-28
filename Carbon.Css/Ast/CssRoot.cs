9c0ae2e93aeed1a20f3147e419b3e7cb6ce1e7d9
Jason Nelson <jason@carbonmade.com>
2014-09-27 23:50:22 -0500

    Add CssRoot

---
+++ Carbon.Css/Ast/CssRoot.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Carbon.Css
+{
+
+	public class CssRoot : CssNode
+	{
+		protected readonly List<CssNode> children;
+
+		public CssRoot()
+			: this(new List<CssNode>()) { }
+
+		public CssRoot(List<CssNode> children)
+			: base(NodeKind.Document)
+		{
+			this.children = children;
+		}
+
+
+		#region Children
+
+		public void RemoveChild(CssNode node)
+		{
+			node.Parent = null;
+
+			children.Remove(node);
+		}
+
+		public void AddChild(CssNode node)
+		{
+			node.Parent = this;
+
+			children.Add(node);
+		}
+
+		public void InsertChild(int index, CssNode node)
+		{
+			node.Parent = this;
+
+			children.Insert(index, node);
+		}
+
+		public override IList<CssNode> Children
+		{
+			get { return children; }
+		}
+
+		#endregion
+
+
+		public override string Text
+		{
+			get { return ""; }
+		}
+
+		public override CssNode CloneNode()
+		{
+			throw new NotImplementedException();
+		}
+
+		
+	}
+}
