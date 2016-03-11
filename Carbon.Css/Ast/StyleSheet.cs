c528e594730e5ae462a4979037a13ea771dbe1db
Jason Nelson <jason@carbonmade.com>
2016-03-10 19:58:27 -0600

    Add inline import helper

--- Carbon.Css/Ast/StyleSheet.cs
+++ Carbon.Css/Ast/StyleSheet.cs
@@ -44,13 +44,7 @@ namespace Carbon.Css
 
             foreach (var node in parser.ReadNodes())
             {
-                if (node.Kind == NodeKind.Assignment)
-                {
-                    var variable = (CssAssignment)node;
-
-                    context.Scope[variable.Name] = variable.Value;
-                }
-                else if (node.Kind == NodeKind.Mixin)
+                if (node.Kind == NodeKind.Mixin)
                 {
                     var mixin = (MixinNode)node;
 
@@ -84,6 +78,21 @@ namespace Carbon.Css
             }
 
             return sheet;
+        }
+
+        public void InlineImports()
+        {
+            if (resolver == null) throw new ArgumentNullException(nameof(resolver));
+
+            foreach (var rule in this.Children.OfType<ImportRule>().ToArray())
+            {
+                if (rule.Url.IsPath)
+                {
+                    ImportInline(rule);
+                }
+
+                this.children.Remove(rule);
+            }
         }
 
         public static StyleSheet FromFile(FileInfo file, CssContext context = null)
@@ -128,6 +137,34 @@ namespace Carbon.Css
             writer.WriteRoot(this);
         }
 
+        public void WriteTo(TextWriter textWriter, IEnumerable<KeyValuePair<string, CssValue>> variables)
+        {
+            var scope = new CssScope();
+
+            foreach (var v in variables)
+            {
+                scope.Add(v.Key, v.Value);
+            }
+
+            var writer = new CssWriter(textWriter, context, resolver);
+
+            writer.SetScope(scope);
+
+            writer.WriteRoot(this);
+        }
+
+        public string ToString(IEnumerable<KeyValuePair<string, CssValue>> variables)
+        {
+            var sb = new StringBuilder();
+
+            using (var sw = new StringWriter(sb))
+            {
+                WriteTo(sw, variables);
+
+                return sb.ToString();
+            }
+        }
+
         public override string ToString()
         {
             var sb = new StringBuilder();
@@ -139,5 +176,81 @@ namespace Carbon.Css
                 return sb.ToString();
             }
         }
+
+
+        #region Helpers
+
+        private void ImportInline(ImportRule rule)
+        {
+            // var relativePath = importRule.Url;
+            var absolutePath = rule.Url.GetAbsolutePath(resolver.ScopedPath);
+
+            // Assume to be scss if there is no extension
+            if (!absolutePath.Contains('.')) absolutePath += ".scss";
+
+            var text = resolver.GetText(absolutePath.TrimStart('/'));
+
+            if (text != null)
+            {
+                if (Path.GetExtension(absolutePath) == ".scss")
+                {
+                    AddChild(new CssComment($"imported: '{absolutePath}"));
+
+                    try
+                    {
+
+                        foreach (var node in Parse(text, context).Children)
+                        {
+                            AddChild(node);
+                        }
+
+                    }
+                    catch (SyntaxException ex)
+                    {
+                        AddChild(new StyleRule("body, html") {
+                           new CssDeclaration("background-color", "red", "important")
+                        });
+
+                        AddChild(new StyleRule("body *") {
+                            { "display", "none" }
+                        });
+                        
+                        AddChild(new CssComment($" --- Syntax error reading '{absolutePath}' : {ex.Message} --- "));
+
+                        var sb = new StringBuilder();
+
+                        if (ex.Lines != null)
+                        {
+                            foreach (var line in ex.Lines)
+                            {
+                                sb.Append(string.Format("{0}. ", line.Number.ToString().PadLeft(5)));
+
+                                if (line.Number == ex.Location.Line)
+                                {
+                                    sb.Append("* ");
+                                }
+
+                                sb.AppendLine(line.Text);
+                            }
+                        }
+
+                        AddChild(new CssComment(sb.ToString()));
+
+                        return;
+                    }
+                }
+                else
+                {
+                    throw new Exception(".css include not supported");
+                    //  writer.Write(text);
+                }
+            }
+            else
+            {
+                AddChild(new CssComment($"NOT FOUND: '{absolutePath}"));
+            }
+        }
+
+        #endregion
     }
 }
