0c78f81701b93f50c74a863a82c9c7437d2b8098
Jason Nelson <jason@carbonmade.com>
2016-03-10 19:57:39 -0600

    Add CssComment node

---
+++ Carbon.Css/Ast/CssComment.cs
@@ -0,0 +1,13 @@
+﻿namespace Carbon.Css
+{
+	public class CssComment : CssNode
+	{
+		public CssComment(string text)
+			: base(NodeKind.Comment)
+        {
+            Text = text;
+		}
+
+		public string Text { get; }
+    }
+}
