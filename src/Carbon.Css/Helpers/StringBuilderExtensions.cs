deca6fa9fd7cec8a7363365420b9789089e8ebe0
Jason Nelson <jason@carbonmade.com>
2018-04-25 17:48:32 -0500

    Remove unnessary wrapper for StringBuilder

---
+++ src/Carbon.Css/Helpers/StringBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Carbon.Css
+{
+    internal static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Extracts the buffered value and clears the string builder.
+        /// </summary>
+        public static string Extract(this StringBuilder sb)
+        {
+            var value = sb.ToString();
+
+            sb.Clear();
+
+            return value;
+        }
+    }
+}
