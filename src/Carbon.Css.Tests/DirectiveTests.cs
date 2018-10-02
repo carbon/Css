0b01bce34d7cedd2ea50ada3038e9a33c7549ac6
Jason Nelson <jason@carbonmade.com>
2018-10-01 21:35:41 -0500

    Add DirectiveTests

---
+++ src/Carbon.Css.Tests/DirectiveTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Carbon.Css.Tests
+{
+    public class DirectiveTests
+    {
+        [Fact]
+        public void ParsePartial()
+        {
+            var sheet = StyleSheet.Parse(@"
+//= partial
+div { color: blue; }
+"); // Prevents standalone compilation
+            
+            Assert.Equal("div { color: blue; }", sheet.ToString());
+        }
+    }
+}
