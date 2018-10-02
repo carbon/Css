b928b1b52972c6d42735ae3649a2855fe43d66d8
Jason Nelson <jason@carbonmade.com>
2018-10-01 21:22:09 -0500

    Add MaskingTests

---
+++ src/Carbon.Css.Tests/Modules/MaskingTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Carbon.Css.Tests
+{
+    public class MaskingTests
+    {
+        [Fact]
+        public void A()
+        {
+            var text = @"
+//= support Safari >= 5
+
+div {
+    mask-image: url('mask.svg');
+}
+
+";
+
+            Assert.Equal(@"
+
+div {
+  -webkit-mask-image: url('mask.svg');
+  mask-image: url('mask.svg');
+}
+".Trim(), StyleSheet.Parse(text).ToString());
+
+
+
+
+        }
+    }
+}
