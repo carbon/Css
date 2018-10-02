f8e301afa8e87402b501b216d8089abcdff41893
Jason Nelson <jason@carbonmade.com>
2018-10-01 21:20:59 -0500

    Add CssFunctionTests

---
+++ src/Carbon.Css.Tests/CssFunctionTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+
+namespace Carbon.Css.Tests
+{
+    public class CssFunctionTests
+    {
+        [Fact]
+        public void ParseFunctionWithLeadingWhitespaceInArgument()
+        {
+            var sheet = StyleSheet.Parse("div { background-color: rgba( 42, 45, 53, 0.7); }");
+
+            Assert.Equal("div { background-color: rgba(42, 45, 53, 0.7); }", sheet.ToString());
+        }
+    }
+}
