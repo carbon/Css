d3a15a6385a853cb539b9850b4c27928b723f4ee
Jason Nelson <jason@carbonmade.com>
2018-10-04 21:47:42 -0500

    Add ReadOnlySpanExtensions

---
+++ src/Carbon.Css/Helpers/ReadOnlySpanExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Carbon.Css.Helpers
+{
+    internal static class ReadOnlySpanExtensions
+    {
+        public static double ReadNumber(this ReadOnlySpan<char> text, out int read)
+        {
+            read = 0;
+
+            while (char.IsDigit(text[read]) || text[read] == '.')
+            {
+                read++;
+            }
+
+            return double.Parse(text.Slice(0, read).ToString());
+        }
+    }
+}
