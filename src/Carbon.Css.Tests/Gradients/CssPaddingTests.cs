14ebfe3c82075b4b5275b94fb4556cfb0aa0af16
Jason Nelson <jason@carbonmade.com>
2018-11-05 17:53:15 -0600

    Add CssPadding tests

---
+++ src/Carbon.Css.Tests/Gradients/CssPaddingTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Carbon.Css.Tests
+{
+    public class CssPaddingTests
+    {
+        [Fact]
+        public void A()
+        {
+            var a = CssPadding.Parse("10px 20%");
+
+            Assert.Equal("10px", a.Top.ToString());
+            Assert.Equal("20%", a.Left.ToString());
+            Assert.Equal("10px", a.Bottom.ToString());
+            Assert.Equal("20%", a.Right.ToString());
+        }
+
+        [Fact]
+        public void B()
+        {
+            var a = CssPadding.Parse("10px");
+
+            Assert.Equal("10px", a.Top.ToString());
+            Assert.Equal("10px", a.Left.ToString());
+            Assert.Equal("10px", a.Bottom.ToString());
+            Assert.Equal("10px", a.Right.ToString());
+        }
+    }
+}
