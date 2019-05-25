1035d72520b8355197f16c6242a108086a0b044c
Jason Nelson <jason@carbonmade.com>
2019-05-25 13:41:26 -0500

    Format TextHelper

---
+++ src/Carbon.Css/Helpers/LineInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Carbon.Css.Helpers
+{
+    public readonly struct LineInfo
+    {
+        public LineInfo(int number, string text)
+        {
+            Number = number;
+            Text = text;
+        }
+
+        public int Number { get; }
+
+        public string Text { get; }
+    }
+}
--- src/Carbon.Css/Helpers/TextHelper.cs
+++ src/Carbon.Css/Helpers/TextHelper.cs
@@ -12,61 +12,46 @@ namespace Carbon.Css.Helpers
             int i = 1;
             int line = 1;
             int column = 1;
-            int charCode = 0;
+            int charCode;
 
-            using (var reader = new StringReader(text))
+            using var reader = new StringReader(text);
+
+            while ((charCode = reader.Read()) != -1)
             {
-                while ((charCode = reader.Read()) != -1)
+                var c = (char)charCode;
+
+                if (c == '\n')
                 {
-                    var c = (char)charCode;
-
-                    if (c == '\n')
-                    {
-                        line++;
-                        column++;
-                    }
-
-                    i++;
-
-                    if (i == position) break;
+                    line++;
+                    column++;
                 }
 
-                return new SourceLocation(i, line, column);
+                i++;
+
+                if (i == position) break;
             }
+
+            return new SourceLocation(i, line, column);
         }
 
         public static IEnumerable<LineInfo> GetLinesAround(this string text, int number, int window = 0)
         {
-            using (var reader = new StringReader(text))
+            using var reader = new StringReader(text);
+
+            var i = 1;
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
             {
-                var i = 1;
-                string line = null;
-
-                while ((line = reader.ReadLine()) != null)
+                if ((i >= (number - window)) && (i <= (number + window)))
                 {
-                    if ((i >= (number - window)) && (i <= (number + window)))
-                    {
-                        yield return new LineInfo(i, line);
-                    }
-
-                    i++;
-
-                    if (i > (number + window)) break;
+                    yield return new LineInfo(i, line);
                 }
+
+                i++;
+
+                if (i > (number + window)) break;
             }
         }
-    }
-
-    public readonly struct LineInfo
-    {
-        public LineInfo(int number, string text)
-        {
-            Number = number;
-            Text = text;
-        }
-
-        public int Number { get; }
-
-        public string Text { get; }
     }
 }
