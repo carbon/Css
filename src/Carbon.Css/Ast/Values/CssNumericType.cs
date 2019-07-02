1cf6303354ec995b30b6d41497bd27a84a121b77
Jason Nelson <jason@carbonmade.com>
2019-07-02 16:41:35 -0500

    Update CssNumericType to byte

--- src/Carbon.Css/Ast/Values/CssNumericType.cs
+++ src/Carbon.Css/Ast/Values/CssNumericType.cs
@@ -1,13 +1,13 @@
 ﻿namespace Carbon.Css.Ast.Values
 {
-    public enum CssNumericType
+    public enum CssNumericType : byte
     {
-        Length,
-        Angle,
-        Time,
-        Frequency,
-        Resolution,
-        Flex,
-        Percent
+        Length = 1,
+        Angle = 2,
+        Time = 3,
+        Frequency = 4,
+        Resolution = 5,
+        Flex = 6,
+        Percent = 7
     }
 }
