4be193346f3eef4edecb3833ee285471eb4c2b95
Jason Nelson <jason@carbonmade.com>
2019-07-02 16:42:12 -0500

    Format Exceptions

--- src/Carbon.Css/Parser/Exceptions/UnexpectedModeChange.cs
+++ src/Carbon.Css/Parser/Exceptions/UnexpectedModeChange.cs
@@ -4,12 +4,7 @@
     {
         // "Current mode is:" + current + ". Leaving " + mode + "."
 
-        private readonly int position;
-
         public UnexpectedModeChange(LexicalMode currentMode, LexicalMode leavingMode, int position)
-            : base($"Unexpected mode change. Expected '{leavingMode}'. Was {currentMode}.")
-        {
-            this.position = position;
-        }
+            : base($"Unexpected mode change. Expected '{leavingMode}'. Was {currentMode}.", position) { }
     }
 }
--- src/Carbon.Css/Parser/Exceptions/UnexpectedTokenException.cs
+++ src/Carbon.Css/Parser/Exceptions/UnexpectedTokenException.cs
@@ -2,26 +2,24 @@
 {
     public sealed class UnexpectedTokenException : SyntaxException
     {
-        private readonly CssToken token;
-
         public UnexpectedTokenException(LexicalMode mode, CssToken token)
             : base($"Unexpected token reading {mode}. Was '{token.Kind}'.")
         {
-            this.token = token;
+            Token = token;
         }
 
         public UnexpectedTokenException(LexicalMode mode, TokenKind expectedKind, CssToken token)
             : base($"Unexpected token at {token.Position} reading {mode}. Expected '{expectedKind}'. Was '{token.Kind}'.", token.Position)
         {
-            this.token = token;
+            Token = token;
         }
 
         public UnexpectedTokenException(CssToken token)
             : base($"Unexpected token. Was '{token.Kind}:{token.Text}'.", token.Position)
         {
-            this.token = token;
+            Token = token;
         }
 
-        public CssToken Token => token;
+        public CssToken Token { get; }
     }
 }
