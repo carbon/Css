8410f2f6d1b15093afd9b01d40f23ee21cbb0166
Jason Nelson <jason@carbonmade.com>
2020-08-01 16:44:55 -0500

    Add test for unquote function

--- src/Carbon.Css.Tests/CssFunctionTests.cs
+++ src/Carbon.Css.Tests/CssFunctionTests.cs
@@ -6,11 +6,28 @@ namespace Carbon.Css.Tests
     public class CssFunctionTests
     {
         [Fact]
+        public void Unquote()
+        {
+            var sheet = StyleSheet.Parse(
+              @"
+                
+                $t1: ""calc((100% - 600px) / 2)""; 
+
+                div { padding: unquote($t1) }
+            
+            ");
+
+
+            Assert.Equal("div { padding: calc((100% - 600px) / 2); }", sheet.ToString());
+        }
+
+        [Fact]
         public void ParseFunctionWithLeadingWhitespaceInArgument()
         {
             var sheet = StyleSheet.Parse("div { background-color: rgba( 42, 45, 53, 0.7); }");
 
             Assert.Equal("div { background-color: rgba(42, 45, 53, 0.7); }", sheet.ToString());
         }
+
     }
 }
