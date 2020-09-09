febe1674e5ad0154c3abc3ad28086f5918afef40
Jason Nelson <jason@carbonmade.com>
2020-09-09 13:00:05 -0500

    Add CssGap

---
+++ src/Carbon.Css/_/CssGap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Carbon.Css
+{
+    public readonly struct CssGap
+    {
+        public CssGap(double value)
+            : this(new CssUnitValue(value, CssUnitInfo.Px)) { }
+
+        public CssGap(CssUnitValue value)
+        {
+            X = value;
+            Y = value;
+        }
+
+        public CssGap(CssUnitValue x, CssUnitValue y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        [DataMember(Name = "x", Order = 1)]
+        public readonly CssUnitValue X { get; }
+
+        [DataMember(Name = "y", Order = 2)]
+        public readonly CssUnitValue Y { get; }
+
+        public readonly override string ToString()
+        {
+            // 1px 1px
+            // 1px
+
+            return X.Equals(Y)
+                ? X.ToString()
+                : X.ToString() + " " + Y.ToString();
+        }
+
+        public static CssGap Parse(string text)
+        {
+            return Parse(text.AsSpan());
+        }
+
+        // 1 or 2 values
+        public static CssGap Parse(ReadOnlySpan<char> text)
+        {
+            int spaceIndex = text.IndexOf(' ');
+
+            if (spaceIndex > -1)
+            {
+                var x = text.Slice(0, spaceIndex);
+                var y = text.Slice(spaceIndex + 1);
+
+                return new CssGap(CssUnitValue.Parse(x), CssUnitValue.Parse(y));
+            }
+
+            return new CssGap(CssUnitValue.Parse(text));
+        }
+    }
+}
