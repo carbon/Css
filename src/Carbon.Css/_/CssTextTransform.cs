739f6a20e5c2366c3338817600e457ef04525db8
Jason Nelson <jason@carbonmade.com>
2020-09-09 13:00:23 -0500

    Add CssTextTransform

---
+++ src/Carbon.Css/_/CssTextTransform.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Carbon.Css
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum CssTextTransform
+    {
+        None         = 0,
+        Capitalize   = 1,
+        LowerCase    = 2,
+        UpperCase    = 3,
+        FullWidth    = 4,
+        FullSizeKana = 5
+    }
+}
