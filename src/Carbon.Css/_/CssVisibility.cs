825067ea260a59b3a296aaadbd8021905d658611
Jason Nelson <jason@carbonmade.com>
2020-09-09 13:00:28 -0500

    Add CssVisibility

---
+++ src/Carbon.Css/_/CssVisibility.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Carbon.Css
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum CssVisibility : byte
+    {
+        Visible  = 1,
+        Hidden   = 2,
+        Collapse = 3
+    }
+}
