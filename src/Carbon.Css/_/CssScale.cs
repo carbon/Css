6e1bfe15edad0a108d9260bd929838035ce265ba
Jason Nelson <jason@carbonmade.com>
2020-09-09 13:00:16 -0500

    Add CssScale

---
+++ src/Carbon.Css/_/CssScale.cs
@@ -0,0 +1,19 @@
+﻿namespace Carbon.Css
+{
+    internal readonly struct CssScale
+    {
+        public CssScale(CssValue x, CssValue y, CssValue z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        // e.g. 100px
+        public readonly CssValue X { get; }
+
+        public readonly CssValue Y { get; }
+
+        public readonly CssValue Z { get; }
+    }
+}
