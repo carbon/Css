fccec722430ffd8877a1240b374ec507f9641c31
Jason Nelson <jason@carbonmade.com>
2020-09-12 18:44:34 -0500

    Improve Thickness test coverage

--- src/Carbon.Css.Tests/ThicknessTests.cs
+++ src/Carbon.Css.Tests/ThicknessTests.cs
@@ -7,6 +7,28 @@ namespace Carbon.Css.Tests
         [Fact]
         public void A()
         {
+            var edge = Thickness.Parse("100px 200px");
+
+            Assert.Equal("100px", edge.Top.ToString());
+            Assert.Equal("100px", edge.Bottom.ToString());
+            Assert.Equal("200px", edge.Left.ToString());
+            Assert.Equal("200px", edge.Right.ToString());
+        }
+
+        [Fact]
+        public void B()
+        {
+            var edge = Thickness.Parse("1 2 3 4");
+
+            Assert.Equal("1", edge.Top.ToString());
+            Assert.Equal("2", edge.Left.ToString());
+            Assert.Equal("3", edge.Bottom.ToString());
+            Assert.Equal("4", edge.Right.ToString());
+        }
+
+        [Fact]
+        public void C()
+        {
             var a = Thickness.Parse("10px 20%");
 
             Assert.Equal("10px", a.Top.ToString());
@@ -18,7 +40,7 @@ namespace Carbon.Css.Tests
         }
 
         [Fact]
-        public void B()
+        public void D()
         {
             var a = Thickness.Parse("10px");
 
