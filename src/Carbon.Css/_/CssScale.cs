162b10e7e615451a239b2ea39f7fed7e365f9a27
Jason Nelson <jason@carbonmade.com>
2021-10-02 17:09:48 -0700

    Implement GetHashCode on CssScale

--- src/Carbon.Css/_/CssScale.cs
+++ src/Carbon.Css/_/CssScale.cs
@@ -12,16 +12,26 @@ internal readonly struct CssScale : IEquatable<CssScale>
     }
 
     // e.g. 100px
-    public readonly CssValue X { get; }
+    public CssValue X { get; }
 
-    public readonly CssValue Y { get; }
+    public CssValue Y { get; }
 
-    public readonly CssValue Z { get; }
+    public CssValue Z { get; }
 
     public bool Equals(CssScale other)
     {
         return X.Equals(other.X)
             && Y.Equals(other.Y)
             && Z.Equals(other.Z);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is CssScale other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y, Z);
     }
 }
