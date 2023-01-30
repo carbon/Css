10c44c999cdb4ad35f53e83bba46fd38d6bc7439
Jason Nelson <jason@carbonmade.com>
2023-01-30 14:34:48 -0800

    Optimize ThicknessJsonConverter

---
+++ src/Carbon.Css/Serialization/ThicknessConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Carbon.Css.Serialization;
+
+public sealed class ThicknessJsonConverter : JsonConverter<Thickness>
+{
+    [SkipLocalsInit]
+    public override Thickness Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        int length = reader.HasValueSequence
+            ? checked((int)reader.ValueSequence.Length)
+            : reader.ValueSpan.Length;
+
+        Span<char> buffer = length < 32
+            ? stackalloc char[32]
+            : new char[length];
+
+        ReadOnlySpan<char> text = buffer.Slice(0, reader.CopyString(buffer));
+
+        return Thickness.Parse(text);
+    }
+
+    public override void Write(Utf8JsonWriter writer, Thickness value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
