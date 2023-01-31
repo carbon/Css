352418a1f658dbb06fa85934c22c071fe83a552f
Jason Nelson <jason@carbonmade.com>
2023-01-31 10:22:15 -0800

    Add CssColorType

---
+++ src/Carbon.Css/Ast/Values/CssColorType.cs
@@ -0,0 +1,10 @@
+﻿namespace Carbon.Css;
+
+public enum CssColorType
+{
+    Hsl   = 1,
+    Hwb   = 2,
+    Lab   = 3, // Safari 15+ Chrome 111+
+    Lch   = 4, // Safari 15+ Chrome 111+
+    OkLab = 5
+}
