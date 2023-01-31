544733a3fa372a2de5f8a8d25f07fc3fcf2a116f
Jason Nelson <jason@carbonmade.com>
2023-01-30 17:17:48 -0800

    Improve CssUnitValue parsing

--- src/Carbon.Css/Serialization/CssUnitValueConverter.cs
+++ src/Carbon.Css/Serialization/CssUnitValueConverter.cs
@@ -1,4 +1,4 @@
-﻿using System.Buffers;
+﻿using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -6,9 +6,20 @@ namespace Carbon.Css.Serialization;
 
 public sealed class CssUnitValueConverter : JsonConverter<CssUnitValue>
 {
+    [SkipLocalsInit]
     public override CssUnitValue Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        return CssUnitValue.Parse(reader.HasValueSequence ? reader.ValueSequence.ToArray() : reader.ValueSpan);
+        int length = reader.HasValueSequence
+            ? checked((int)reader.ValueSequence.Length)
+            : reader.ValueSpan.Length;
+
+        scoped Span<byte> buffer = length < 32
+            ? stackalloc byte[32]
+            : new byte[length];
+
+        ReadOnlySpan<byte> text = buffer.Slice(0, reader.CopyString(buffer));
+
+        return CssUnitValue.Parse(text);
     }
 
     public override void Write(Utf8JsonWriter writer, CssUnitValue value, JsonSerializerOptions options)
