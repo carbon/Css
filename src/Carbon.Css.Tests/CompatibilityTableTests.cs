33472d87d665717b4097bcb330be77f99412fd89
Jason Nelson <jason@carbonmade.com>
2023-09-06 15:30:39 -0700

    Add CompatibilityTableTests

---
+++ src/Carbon.Css.Tests/CompatibilityTableTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Carbon.Css.Tests;
+
+public class CompatibilityTableTests
+{
+    [Fact]
+    public void TestIsDefined()
+    {
+        Assert.False(default(CompatibilityTable).IsDefined);
+        Assert.True(new CompatibilityTable(1, 2, 3, 4).IsDefined);
+    }
+}
