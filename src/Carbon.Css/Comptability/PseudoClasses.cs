b278bb52c647f69296d4693c9b11d87070b119f0
Jason Nelson <jason@carbonmade.com>
2023-10-19 16:53:49 -0700

    Breakout pseudo element names

--- src/Carbon.Css/Comptability/PseudoClasses.cs
+++ src/Carbon.Css/Comptability/PseudoClasses.cs
@@ -5,15 +5,26 @@ namespace Carbon.Css;
 public static class PseudoClassNames
 {
     private static readonly FrozenSet<string> s_items = FrozenSet.ToFrozenSet([
-        "active", "after", "any", "any-link", "before", "checked", "cue", "default", "dir", "disabled", "empty", "enabled",
-        "first", "first-letter", "first-line", "first-child", "first-of-type", "fullscreen", "focus", "focus-within",
-        "hover", "indeterminate", "in-range", "invalid", "lang", "last-child", "last-of-type", "left", "link",
-        "not", "nth-child", "nth-last-child", "nth-last-of-type", "only-child", "only-of-type", "optional",
-        "out-of-range",
-        "placeholder-shown", "read-only", "read-write", "required", "right", "root", "scope",
-        "target", "valid", "visited"
+        "active", "any", "any-link", "autofill",
+        "blank", "buffering",
+        "checked", "current",
+        "default", "dir", "disabled", 
+        "empty", "enabled",
+        "first", "first-line", "first-child", "first-of-type", "fullscreen", "focus", "focus-within",
+        "has", "host", "hover",
+        "indeterminate", "in-range", "invalid", "is",
+        "lang", "last-child", "last-of-type", "left", "link", "local-link",
+        "modal",
+        "not", "nth-child", "nth-last-child", "nth-last-of-type",
+        "only-child", "only-of-type", "optional", "out-of-range",
+        "paused", "placeholder-shown", "playing", "popover-open",
+        "read-only", "read-write", "required", "right", "root", 
+        "scope", "seeking", "stalled",
+        "target",
+        "user-invalid", "user-valid",
+        "valid", "visited", "volume-locked",
+        "where"
     ]);
-
 
     public static bool Contains(string name) => s_items.Contains(name);
 }
---
+++ src/Carbon.Css/Comptability/PseudoElementNames.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Frozen;
+
+namespace Carbon.Css;
+
+public static class PseudoElementNames
+{
+    private static readonly FrozenSet<string> s_items = FrozenSet.ToFrozenSet([
+        "after",
+        "backdrop",
+        "before",
+        "cue",
+        "cue-region",
+        "file-selector-button",
+        "first-letter",
+        "first-line",
+        "grammar-error",
+        "highlight",
+        "marker",
+        "part",
+        "placeholder",
+        "selection",
+        "slotted",
+        "spelling-error",
+        "target-text",
+        "view-transition",
+        "view-transition-group"
+    ]);
+
+    public static bool Contains(string name) => s_items.Contains(name);
+}
