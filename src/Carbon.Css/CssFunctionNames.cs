fd696756c4fd2429ddc48f234546c410527a515e
Jason Nelson <jason@carbonmade.com>
2023-10-20 11:36:22 -0700

    Add CssFunctionNames

---
+++ src/Carbon.Css/CssFunctionNames.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Frozen;
+
+namespace Carbon.Css;
+
+internal static class CssFunctionNames
+{
+    private static readonly FrozenSet<string> s_items = FrozenSet.ToFrozenSet([
+        "attr",
+        "calc",
+        "cubic-bezier",
+        "var",
+        "round",
+        "mod",
+        "rem", 
+        "sin",
+        "cos",
+        "tag",
+        "asin",
+        "acos",
+        "atan",
+        "atan2",
+        "sqrt",
+        "hypot",
+        "log",
+        "exp",
+        "abs",
+        "sign"
+    ]);
+
+    public static bool Contains(string name) => s_items.Contains(name);
+}
